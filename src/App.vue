c3d1afb9b7c437fa2e4a720547fa3b25baae4508
scarlett-troncoso <scar.labussieri@gmail.com>
2024-01-31 17:43:22 +0100

    change methods addTask

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,11 @@ export default {
 
   data() {
     return {
-      newTask: '',
+
+      newText: {
+        text: '',
+        done: false,
+      },
 
       toDoList: [
         {
@@ -34,12 +38,9 @@ export default {
     },
 
     addTask() {
-      //const { text, done } = toDoList;
-      console.log('Aggiunge task:', this.newTask);
-
-      //this.toDoList.map(item => item.text.push(this.newTask));
-      this.toDoList.unshift(this.newTask);
-      this.newTask = '';
+      console.log('Aggiunge task:', this.newText);
+      this.toDoList.unshift(this.newText);
+      this.newText = '';
     }
   }
 }
@@ -47,37 +48,34 @@ export default {
 
 <template>
   <header>
-    <h1>To Do List</h1>
+    <h1 class="m-3">To Do List</h1>
   </header>
 
   <main>
+    <div class="card w-50 m-auto">
+      <div class="input-group mt-3 align-items-center justify-content-center">
+        <Button for="addTask" class="btn btn-secondary" @click="addTask">Add task</Button>
+        <input type="text" id="addTask" v-model="newText.text">
+      </div>
 
-    <div class="m-1">
-      <label for="addTask" class="m-1" @click="addTask">Add task</label>
-      <input type="text" id="addTask" v-model="newTask">
+      <ul v-if="toDoList.length" class="ul-list">
+        <li v-for="(item, index) in  toDoList" class="itemList" :class="{ sbarra: item.done }">
+          <!--<input type="checkbox" @change="item.done = true">-->
+          <!--<p :class="item.done ? 'sbarra' : ''"> --> <!--potrebbe essere anche cosí come ho fatto prima-->
+          <!--mette la class in item.done sólo in quelli che sono true-->
+          {{ item.text }}
+
+          <span v-on:click="removeItem(index)">
+            <i class="fa-regular fa-circle-xmark m-1"></i>
+          </span>
+        </li>
+      </ul>
+
+      <p v-else class="ul-list">
+        No task to do
+      </p>
     </div>
-
-    <ul v-if="toDoList.length > 0">
-      <li v-for="(item, index) in  toDoList" class="itemList" :class="{ sbarra: item.done }">
-        <!--<input type="checkbox" @change="item.done = true">-->
-        <!--<p :class="item.done ? 'sbarra' : ''"> --> <!--potrebbe essere anche cosí come ho fatto prima-->
-        <!--mette la class in item.done sólo in quelli che sono true-->
-        {{ item.text }}
-
-        <span v-on:click="removeItem(index)">
-          <i class="fa-regular fa-circle-xmark m-1"></i>
-        </span>
-      </li>
-    </ul>
-
-    <p v-else>
-      No task to do
-    </p>
   </main>
-
-  <footer>
-    <p>Footer</p>
-  </footer>
 </template>
 
 <style></style>
