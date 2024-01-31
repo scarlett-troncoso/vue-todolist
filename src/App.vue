5591a195f9106c129fc5f3552314d22782aff177
scarlett-troncoso <scar.labussieri@gmail.com>
2024-01-31 09:57:10 +0100

    add methods add task

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ export default {
 
   data() {
     return {
+      newTask: '',
 
       toDoList: [
         {
@@ -18,6 +19,10 @@ export default {
           text: 'Compito di martedí',
           done: true,
         },
+        {
+          text: 'Colloquio',
+          done: true,
+        },
       ]
     }
   },
@@ -26,6 +31,15 @@ export default {
     removeItem(index) {
       console.log('rimovere questo item', index);
       this.toDoList.splice(index, 1);
+    },
+
+    addTask() {
+      //const { text, done } = toDoList;
+      console.log('Aggiunge task:', this.newTask);
+
+      //this.toDoList.map(item => item.text.push(this.newTask));
+      this.toDoList.unshift(this.newTask);
+      this.newTask = '';
     }
   }
 }
@@ -39,21 +53,20 @@ export default {
   <main>
 
     <div class="m-1">
-      <label for="addTask" class="m-1">Add task</label>
-      <input type="text" id="addTask">
+      <label for="addTask" class="m-1" @click="addTask">Add task</label>
+      <input type="text" id="addTask" v-model="newTask">
     </div>
+
     <ul v-if="toDoList.length > 0">
-      <li v-for="(item, index) in  toDoList " class="itemList">
+      <li v-for="(item, index) in  toDoList" class="itemList" :class="{ sbarra: item.done }">
         <!--<input type="checkbox" @change="item.done = true">-->
         <!--<p :class="item.done ? 'sbarra' : ''"> --> <!--potrebbe essere anche cosí come ho fatto prima-->
-        <p :class="{ sbarra: item.done }"> <!--mette la class in item.done sólo in quelli che sono true-->
-          {{ item.text }}
+        <!--mette la class in item.done sólo in quelli che sono true-->
+        {{ item.text }}
 
-          <span v-on:click="removeItem(index)">
-            <i class="fa-regular fa-circle-xmark m-1"></i>
-          </span>
-        </p>
-
+        <span v-on:click="removeItem(index)">
+          <i class="fa-regular fa-circle-xmark m-1"></i>
+        </span>
       </li>
     </ul>
 
